4991d1da23ea395cd047f03549c6105bf72f19cd
ghousshah <ghous.shah91@gmail.com>
2017-03-07 09:39:56 +0500

    my 2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert ("hello world")
+var a;
+
+a = +prompt("Grade Calculater","Enter Your Marks");
+if(a>=90){
+    alert("You Got A-1 Grade");
+}
+else if(a>=80){
+    alert("You Got A Grade");
+}
+else if(a>=70){
+    alert("You Got B Grade");
+}
+else if(a>=60){
+    alert("You Got C Grade");
+}
+else if(a>=50){
+    alert("You Got D Grade");
+}
+else if(a<=49){
+    alert("Failed '.' ");
+}
